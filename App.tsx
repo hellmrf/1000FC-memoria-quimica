3177c9ff219be52c6169802556077f42a75ed799
Heliton Martins <helitonmrf@gmail.com>
2021-05-22 17:41:22 -0300

    :sparkles:  Adding custom font load
    A tela de preload e mostrada enquanto as fontes carregam. No futuro e possivel que existam outras tarefas para esse momento.

--- App.tsx
+++ App.tsx
@@ -2,8 +2,16 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import MainStack from './src/stacks/MainStack';
+import { useFonts, Lato_700Bold } from '@expo-google-fonts/lato';
+import Preload from './src/screens/Preload';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Lato_700Bold,
+  });
+  if (!fontsLoaded) return <Preload />;
+
+  console.log('Ok');
   return (
     <NavigationContainer>
       <MainStack />
