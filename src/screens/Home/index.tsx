1e7f244dcc823f5fbfae1e1fec181672a58eda2a
Nayron Morais <nayronmorais@gmail.com>
2021-06-01 16:09:55 -0300

    :construction:  The main screen now is responsive.
    The main screen was changed to have a responsive behavior.
    
    Closes #1

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
+import { View, useWindowDimensions } from 'react-native';
 
 import PlayButton from '../../assets/play_button.svg';
+import { app_primary_title_size } from '../../dimensions/text';
 
 import {
   AtomAnimation,
@@ -12,27 +14,69 @@ import {
   TitleOfTheGameText,
 } from './styles';
 
+/// Must be sum up 1.
+const SCREEN_TITLE_PERC = 0.15;
+const SCREEN_ATOM_PERC = 0.55;
+const SCREEN_PLAY_PERC = 0.3;
+
 export default () => {
+  let screen_width = useWindowDimensions().width;
+  let screen_height = useWindowDimensions().height;
+
+  const atomAnimationPercSize = 0.8;
+  const atomBorderPercSize = 0.9;
+  const atomMaxSize = Math.min(SCREEN_ATOM_PERC * screen_height, screen_width);
+
+  const playButtonPercSize = 0.75;
+  const playButtonSize =
+    playButtonPercSize * (SCREEN_PLAY_PERC * screen_height);
+
   return (
     <Container>
       <Background
         start={{ x: 1, y: 1 }}
         end={{ x: 0.0, y: 0 }}
         colors={['#13f3cbff', '#13f3cb60', '#13f3cb00']}>
-        <TitleOfTheGame>
-          <TitleOfTheGameText>Jogo</TitleOfTheGameText>
-          <TitleOfTheGameText>da</TitleOfTheGameText>
-          <TitleOfTheGameText bold>Memória</TitleOfTheGameText>
-        </TitleOfTheGame>
-        <AtomAnimationBorder>
-          <AtomAnimation
-            style={{ resizeMode: 'stretch' }}
-            source={require('../../assets/atom_animation.gif')}
-          />
-        </AtomAnimationBorder>
-        <PlayButtonArea>
-          <PlayButton height={120} width={120} fill="white" />
-        </PlayButtonArea>
+        {/* Container para o título */}
+        <View flex={SCREEN_TITLE_PERC} zIndex={1} justifyContent="center">
+          <TitleOfTheGame
+            marginBottom={-1.25 * (SCREEN_TITLE_PERC * screen_height)}>
+            <TitleOfTheGameText fontSize={app_primary_title_size}>
+              Jogo
+            </TitleOfTheGameText>
+            <TitleOfTheGameText fontSize={app_primary_title_size}>
+              da
+            </TitleOfTheGameText>
+            <TitleOfTheGameText bold fontSize={app_primary_title_size}>
+              Memória
+            </TitleOfTheGameText>
+          </TitleOfTheGame>
+        </View>
+        {/* Container para a animação */}
+        <View
+          flex={SCREEN_ATOM_PERC}
+          justifyContent="center"
+          alignItems="center">
+          <AtomAnimationBorder diameter={atomBorderPercSize * atomMaxSize}>
+            <AtomAnimation
+              source={require('../../assets/atom_animation.gif')}
+              diameter={atomAnimationPercSize * atomMaxSize}
+            />
+          </AtomAnimationBorder>
+        </View>
+        {/* Container para o butão play */}
+        <View
+          flex={SCREEN_PLAY_PERC}
+          justifyContent="flex-start"
+          alignItems="center">
+          <PlayButtonArea diameter={playButtonSize}>
+            <PlayButton
+              height={playButtonSize}
+              width={playButtonSize}
+              fill="white"
+            />
+          </PlayButtonArea>
+        </View>
       </Background>
     </Container>
   );
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -11,42 +11,40 @@ export const Container = styled.View`
 
 export const Background = styled(LinearGradient)`
   flex: 1;
-  width: 100%;
-  height: 100%;
-  flex-direction: column;
-  justify-content: space-evenly;
-  align-items: center;
 `;
 
-export const AtomAnimationBorder = styled.View`
-  width: 75%;
-  height: ${windowWidth * 0.75}px;
+export const AtomAnimationBorder = styled.View<{ diameter: number }>`
+  width: ${props => props.diameter}px;
+  height: ${props => props.diameter}px;
   background-color: #ffffff77;
-  border-radius: 150px;
+  border-radius: ${props => props.diameter / 2}px;
   justify-content: center;
   align-items: center;
 `;
 
-export const AtomAnimation = styled.Image`
-  width: 87%;
-  height: 87%;
+export const AtomAnimation = styled.Image<{ diameter?: number }>`
+  width: ${props => props.diameter + 'px' || '87%'};
+  height: ${props => props.diameter + 'px' || '87%'};
+  resize-mode: contain;
 `;
 
-export const PlayButtonArea = styled.TouchableOpacity`
+export const PlayButtonArea = styled.TouchableOpacity<{ diameter: number }>`
   background-color: #ffffff33;
-  border-radius: 60px;
+  border-radius: ${props => props.diameter / 2 + 'px'};
 `;
 
-export const TitleOfTheGame = styled.View`
-  justify-content: center;
+export const TitleOfTheGame = styled.View<{ marginBottom?: number }>`
+  justify-content: flex-end;
   align-items: center;
-  margin-bottom: -140px;
-  z-index: 5;
+  margin-bottom: ${props => props.marginBottom || 0}px;
 `;
 
-export const TitleOfTheGameText = styled.Text<{ bold?: boolean }>`
+export const TitleOfTheGameText = styled.Text<{
+  bold?: boolean;
+  fontSize: number;
+}>`
   font-family: ${props => (props.bold ? 'LoveloBlack' : 'LoveloLineBold')};
-  font-size: 54px;
+  font-size: ${props => props.fontSize}px;
   color: white;
   text-transform: uppercase;
   text-align: center;
