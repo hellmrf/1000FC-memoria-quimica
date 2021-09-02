393d2d810ab510dfd7fdbc0cc52a0ada97bd97f1
Heliton Martins <helitonmrf@gmail.com>
2021-09-02 12:19:16 -0300

    :sparkles:  Added currentPlayer to state
    Now the current player's avatar is selected accordingly.

--- src/screens/Game/index.tsx
+++ src/screens/Game/index.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, View } from 'react-native';
 import theme from '../../themes';
+import { range } from '../../utils/misc';
 
 import HitsCounter from '../../components/HitsCounter';
 import GameCard from '../../components/GameCard';
@@ -22,26 +23,30 @@ import LightBulbIcon from '../../assets/icons/light_bulb.svg';
 /// Repeats `Element` for `length` times, adding a `key` prop.
 function repeat_element(
   length: number,
-  Element: React.FC
+  Element: React.FC<any>,
+  properties: any = {}
 ): Array<React.ReactNode> {
-  return [...Array(length).keys()].map(x => <Element key={x} />);
+  return range(length).map(x => <Element key={x} {...properties} />);
 }
 
 export default () => {
-  const cards = repeat_element(4 * 4, GameCard);
+  const numberOfPlayers = 5; // TODO: get from props
+  const numberOfCards = 4 * 4;
+
+  const [activePlayer, setActivePlayer] = React.useState(0);
+  const [points, setPoints] = React.useState(1000);
+  const nextPlayer = () => setActivePlayer((activePlayer + 1) % numberOfPlayers);
+
+  const avatars = range(numberOfPlayers).map(x => (
+    <AvatarSelector key={x} active={activePlayer === x} />
+  ));
 
   return (
     <Container>
       <GameHeader>
         <GameHeaderLeft>
-          <Avatars>
-            <AvatarSelector />
-            <AvatarSelector />
-            <AvatarSelector />
-            <AvatarSelector />
-            <AvatarSelector active />
-          </Avatars>
-          <PointsText>1000 pontos</PointsText>
+          <Avatars>{avatars}</Avatars>
+          <PointsText>{points} pontos</PointsText>
         </GameHeaderLeft>
         <GameHeaderRight>
           <LightBulbCircle>
@@ -58,7 +63,11 @@ export default () => {
         }}>
         SUA VEZ!
       </Text>
-      <GameBoard>{cards}</GameBoard>
+      <GameBoard>
+        {repeat_element(numberOfCards, GameCard, {
+          onPress: nextPlayer,
+        })}
+      </GameBoard>
       <GameFooter>
         <HitsCounter hit counter={0} />
         <HitsCounter hit={false} counter={0} />
